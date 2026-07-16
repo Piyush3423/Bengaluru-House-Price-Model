aedac0389a7ef7e31add0449f5cd6b146bd6f588
Piyush_sael <vatspiyush3423@gmail.com>
2026-07-16 16:29:30 +0530

    Refactor prediction logic and enhance response structure in FastAPI application

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
+import pickle
+import json
+import numpy as np
 from fastapi import FastAPI
+from fastapi import HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
+
+#Load the model
+model = pickle.load(open('Benglore_House_Price_model.pickle','rb'))
+data_columns = json.load(open('columns.json', 'r'))['data_columns']
+class HouseData(BaseModel):
+    location : str
+    total_sqft : float
+    bath : int
+    bhk : int
+
+class PredictionResponse(BaseModel):
+    predicted_price : float
+    currency : str
+    message : str
+    
+    
 @app.get("/")
 def home():
     return {"message": "Welcome to Bengaluru House Price Prediction API"}
 
-class HouseData(BaseModel):
-    location : str
-    total_sqrt : float
-    bath : int
-    bhk : int
+@app.post("/predict", response_model = PredictionResponse)
+def predict(data: HouseData):
+    x = np.zeros(len(data_columns))
+
+    x[0] = data.total_sqft
+    x[1] = data.bath
+    x[2] = data.bhk
+    
+    if data.location in data_columns:
+        loc_idx = data_columns.index(data.location)
+        x[loc_idx] = 1
+        
+    prediction = model.predict([x])[0]
+    
+    return {
+        "predicted_price": round(prediction, 2),
+        "currency":'Lakhs',
+        "message": 'Prediction generated successfully.'
+    }
